1bafe9a6d694088cbe312ec69611c9e32e29afd9
theobeo2004 <thenhuet2004@gmail.com>
2025-09-08 15:06:18 +0700

    feat: convert 2D dataframe to 3D tensor for spatiotemporal representation

--- training_scripts/load_data.py
+++ training_scripts/load_data.py
@@ -2,6 +2,26 @@ import pandas as pd
 import numpy as np
 
 
+def convert3dtensor(nodes, df: pd.DataFrame):
+    """ 
+    Convert 2D sp500 dataframe into 3D shape tensor 
+    which is in form (n_nodes, n_timestamps, n_features)
+    
+    Args:
+
+        - df (pd.DataFrame): Dataframe needed to be converted
+        - nodes (np.ndarray): A np.ndarry consists of stock labels
+    Return:
+        (np.ndarray): Data returned in 3D shape
+    """
+    df.set_index(['Symbol', 'Date'], inplace=True)
+    
+    return np.stack(
+        [df.loc[node].values.T for node in nodes],
+        axis=0
+    ).transpose(0, 2, 1)
+
+
 def load_data(train=True):
     
     """ 
@@ -12,10 +32,13 @@ def load_data(train=True):
     Args:
         - train (bool): If True, return 4 folds, each fold consists of both training & validation sets. 
         Otherwise, it would return test set with entire training set.
+    Return:
+        (list): Set of couples, each couple consists of 2 sets, the second following the first by time
     """
     
-    df = pd.read_csv('../input/clear_sp500.csv', index_col=0)
+    df = pd.read_csv('../input/clear_sp500.csv')
     df['Date'] = pd.to_datetime(df['Date'])
+    nodes = df['Symbol'].unique()
     
     timelines = [
         pd.Timestamp(year=2025, month=4, day=1, tz='UTC'),
@@ -37,14 +60,16 @@ def load_data(train=True):
                         
                         timelines[i] + predicted_range)].copy().reset_index(drop=True)
             
-            folds.append((train_df, valid_df))
+            folds.append((convert3dtensor(nodes, train_df), 
+                          convert3dtensor(nodes, valid_df)))
     
     else:
         train_df = df[df['Date'] < timelines[4]].copy().reset_index(drop=True)
         
         test_df = df[df['Date'] >= timelines[4]].copy().reset_index(drop=True)
         
-        folds.append((train_df, test_df))
+        folds.append((convert3dtensor(nodes, train_df),
+                     convert3dtensor(nodes, test_df)))
         
     return folds
         
